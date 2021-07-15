5bf72f5fd6e5bcf1781d0f06f2ed1aecae55fd7b
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2021-07-15 17:42:02 +0700

    Login google ok

--- resources/views/layouts/newapp.blade.php
+++ resources/views/layouts/newapp.blade.php
@@ -52,13 +52,12 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                            <li class="nav-item ">
+                                <a id="navbarDropdown" class="nav-link " href="#" role="button" data-toggle="" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Selamat Datang,   {{ Auth::user()->name }}
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                            </li>
+                            <a class="btn btn-dark" style="margin-left:10px;" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
@@ -67,8 +66,6 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-                                </div>
-                            </li>
                         @endguest
                     </ul>
                 </div>
--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.newapp')
 
-<head>
-    <title>
-    </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap
-/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="an
-onymous">
-</head>
-<body style="margin-top:20px;">
+@section('content')
+
     <div class="container">
         @if(session('sukses'))
         <div class="alert alert-success" role="alert">
@@ -94,5 +87,5 @@ onymous"></script>
 onymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="an
 onymous"></script>
-</body>
-</html>
+
+@endsection
